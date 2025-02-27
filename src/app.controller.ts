aabfd9ce1ab007688557d3f975b6b45824250936
CabosManuel <cabos.apolaya@gmail.com>
2025-02-26 21:47:59 -0500

    Controllers and first custom endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,21 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // Enpoint vacío por defecto localhost:3000/
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // Endpoint localhost:3000/new
+  @Get('new')
+  newEndpoint(): string {
+    return 'New endpoint in NestJS!';
+  }
+
+  // Endpoint localhost:3000/home
+  @Get('/home/')
+  homePage(): string {
+    return 'Home page';
   }
 }

604e5d6706c1df816092c61b62b7db2cdfc8baba
CabosManuel <cabos.apolaya@gmail.com>
2025-02-26 22:19:30 -0500

    Endpoint Parameters

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -22,4 +22,25 @@ export class AppController {
   homePage(): string {
     return 'Home page';
   }
+
+  // localhost:3000/products/p01
+  @Get('/products/:productId')
+  getProduct(@Param('productId') productId: string): string {
+    return `Product ID: ${productId}`;
+  }
+
+  // localhost:3000/categories/c01/products/p01
+  @Get('/categories/:id/products/:productId')
+  getCategory(
+    @Param('productId') productId: string,
+    @Param('id') id: string,
+  ): string {
+    return `Category ID: ${id} - Product ID: ${productId}`;
+  }
+
+  // localhost:3000/tcategories/c01/tproducts/p01
+  // @Get('/tcategories/:id/tproducts/:productId')
+  // getCategory2(@Param() params: any): string {
+  //   return `Category ID: ${params.id} - Product ID: ${params.productId}`;
+  // }
 }
